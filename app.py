8c356a50db84453daf5733affc13ef6ad889f360
Baldpet <peterdaniel.baldwin@gmail.com>
2020-10-03 14:06:34 +0000

    linked sign up form with the user mongoDB database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 
 if os.path.exists("env.py"):
     import env
@@ -11,11 +11,13 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'family_cookbook'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+'''
 login_manager = LoginManager()
-
+'''
 mongo = PyMongo(app)
+'''
 login_manager.init_app(app)
-
+'''
 
 @app.route('/')
 def home():
@@ -25,6 +27,13 @@ def home():
 @app.route('/SignUp')
 def sign_up():
     return render_template("signup.html")
+
+
+@app.route('/SignUp/user', methods=['POST', 'GET'])
+def sign_up_user():
+    user = mongo.db.users
+    user.insert_one(request.form.to_dict())
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':

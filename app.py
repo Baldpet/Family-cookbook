e45968ebf77334caa9f49184d57fb5795f561026
Baldpet <peterdaniel.baldwin@gmail.com>
2020-10-03 15:27:50 +0000

    added hashing of the passwords and verify that the password is matching

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_login import LoginManager, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 app.config["MONGO_DBNAME"] = 'family_cookbook'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
@@ -32,8 +34,12 @@ def sign_up():
 @app.route('/SignUp/user', methods=['POST', 'GET'])
 def sign_up_user():
     user = mongo.db.users
-    user.insert_one(request.form.to_dict())
-    return redirect(url_for('home'))
+    if (request.form.get('password')) == (request.form.get('password_check')):
+        hash = generate_password_hash(request.form.get('password'))
+        print(hash)
+    else:
+        flash('Error! Your password does not match')
+    return redirect(url_for('sign_up'))
 
 
 if __name__ == '__main__':

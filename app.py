e4140a0dd198b5ea6df2d7034f4bf36ad82d549c
Baldpet <peterdaniel.baldwin@gmail.com>
2020-10-03 13:25:24 +0000

    added the basic sign up html/route and form

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_login import LoginManager
 
 if os.path.exists("env.py"):
     import env
@@ -10,13 +11,20 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'family_cookbook'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+login_manager = LoginManager()
 
 mongo = PyMongo(app)
+login_manager.init_app(app)
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/SignUp')
+def sign_up():
+    return render_template("signup.html")
 
 
 if __name__ == '__main__':

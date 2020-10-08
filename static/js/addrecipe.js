4f149c1a2177e8fedffe17c69fe205446e4e1fc3
Baldpet <peterdaniel.baldwin@gmail.com>
2020-10-08 13:24:48 +0000

    styled the form to be responsive and center aligned the title/submit

--- static/js/addrecipe.js
+++ static/js/addrecipe.js
@@ -1,7 +1,7 @@
 
 /* Allows the buttons to add another ingredient line and remove one if needed */
 $("#addingredient").click(function(){
-    $("#break1").before('<div><div class="input-field col s10"><input name="ingredients" type="text" class="validate required"></div><div class=" col s2"><a class="btn-floating btn-small waves-effect waves-light red remove"><i class="material-icons">remove</i></a></div></div>');
+    $("#break1").before('<div><div class="input-field col s10 m8 l6 offset-m1 offset-l2"><input name="ingredients" type="text" class="validate required"></div><div class="col center-align s2"><a class="btn-floating btn-small waves-effect waves-light red remove"><i class="material-icons">remove</i></a></div></div>');
     $(".remove").click(function(){
         $(this).parent().parent().remove()
     })
@@ -9,7 +9,7 @@ $("#addingredient").click(function(){
 
 /* Allows the buttons to add another method line and remove one if needed */
 $("#addmethod").click(function () {
-    $("#break2").before('<div><div class="input-field col s10"><textarea name="method" type="text" class="materialize-textarea" required></textarea></div><div class=" col s2"><a class="btn-floating btn-small waves-effect waves-light red remove"><i class="material-icons">remove</i></a></div></div>');
+    $("#break2").before('<div><div class="input-field col s10 m8 l6 offset-m1 offset-l2"><textarea name="method" type="text" class="materialize-textarea" required></textarea></div><div class="center-align col s2"><a class="btn-floating btn-small waves-effect waves-light red remove"><i class="material-icons">remove</i></a></div></div>');
     $(".remove").click(function(){
         $(this).parent().parent().remove()
     })

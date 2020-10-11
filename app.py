68a810f46bf57ab5d67aeddf88757474bcd3d134
Baldpet <peterdaniel.baldwin@gmail.com>
2020-10-11 14:23:41 +0000

    Adjusted the messages on the index.html page

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def login():
                 flash("Logged in successfully")
                 return redirect(request.args.get("next") or url_for(
                     "home_login", username=user['username']))
-        flash("Wrong username or password")
+        flash("Wrong Username or Password entered, please try again.")
     return redirect(url_for('home'))
 
 

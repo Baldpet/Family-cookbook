66805c12e5973bfe0fe89327bdf993db13161ca0
Baldpet <peterdaniel.baldwin@gmail.com>
2020-10-13 00:50:28 +0000

    Updated the js filter to allow the recipe search to work

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,23 +1,33 @@
-/*$('.filter').submit(function(){
-    ingredient = $(this).attr("id");
-    $('.recipe').addClass('display-none');
-    $("." + ingredient).removeClass('display-none');
-    $("." + mainIngredient).removeClass('display-none');
-})*/
+/* Code taken from Stack Overflow */
+/* Creates the new function icontains that is not case sensitive */
+jQuery.expr[':'].icontains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+/* End of code taken from Stack Overflow */
 
 $('.filter').click(function(){
     let values = $('form').serializeArray()
     let mainIngredient = values[0].value
     let recipeName = values[1].value
-
-    if (mainIngredient === 'none'){
+    let search = "h5:icontains(" + recipeName + ")"
+    if (mainIngredient === 'none' && recipeName == ""){
         $('.recipe').removeClass('display-none');
-    } else {
+    } else if (mainIngredient === 'none'){
+        $('.recipe').addClass('display-none');
+        $(search).parent().parent().parent().parent().parent().removeClass('display-none');
+    } else if (recipeName == ""){
         $('.recipe').addClass('display-none');
         $("." + mainIngredient).parent().parent().parent().removeClass('display-none');
-        
+    } else {
+        $('.recipe').addClass('display-none');
+        $(search).parent().parent().parent().parent().parent().addClass('visible');
+        if ($("." + mainIngredient).parent().parent().parent().hasClass('visible')){
+            $(".visible").removeClass('display-none');
+        } else{
+
+        }
+        $(search).parent().parent().parent().parent().parent().removeClass('visible');
     }
-    console.log(values)
-    console.log(mainIngredient)
-    console.log(recipeName)
 })
